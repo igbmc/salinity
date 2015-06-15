724ad37c219a6700a94fb323194bab1f43ac3720
Julien Seiler <julien.seiler@gmail.com>
2015-06-15 14:15:35 +0200

    removed dependencies to pip in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import os
 
-from pip.req import parse_requirements
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 LONG_DESC = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
-install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-reqs = [str(ir.req) for ir in install_reqs]
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+    requires = f.read().splitlines()
 
 setup(
     name="salinity",
@@ -21,7 +19,7 @@ setup(
     license="GNU GENERAL PUBLIC LICENSE v2",
     url="http://github.com/julozi/salinity",
     zip_safe=False,
-    install_requires=reqs,
+    install_requires=requires,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
